dd48385f6508bc0947ef2d635cf75addb7997169
vicdubbeld <vicdubbeld@gmail.com>
2021-03-17 22:30:55 -0700

    implemented logic

---
+++ main.js
@@ -0,0 +1,3 @@
+const expenses1 = document.querySelector(".title .expenses-value");
+const remaining1 = document.querySelector(".title .remaining-value");
+const income1 = document.querySelector(".title .income-value");
